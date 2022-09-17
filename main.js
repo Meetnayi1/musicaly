7ff0692469a9cd3f691237820e824b809b9718ec
Meetnayi1 <113197012+Meetnayi1@users.noreply.github.com>
2022-09-17 11:04:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -51,6 +51,24 @@ let track_list = [
     image: "D:\music pratice\20220906_125857_0000.png",
     path: "https://oyedjsurendra.com/files/download/id/3695&volume=75&showstop=1&showvolume=1",
   },  
+  {
+    name: "Main Hoon Saath Tere",
+    artist: "Arjit Singh",
+    image: "D:\music pratice\20220906_125857_0000.png",
+    path: "https://ghantalele.com/player/30347",
+  },  
+  {
+    name: "Channa ve ",
+    artist: "Akhil Sachdeva",
+    image: "D:\music pratice\20220906_125857_0000.png",
+    path: "https://mastimusic.com/files/download/id/999",
+  },  
+  {
+    name: " rang lageya",
+    artist: "mohit chauhan",
+    image: "D:\music pratice\20220906_125857_0000.png",
+    path: "https://pwdown.com/113514/Rang%20Lageya%20-%20Mohit%20Chauhan.mp3",
+  },  
 ];
 
 function random_bg_color() {
@@ -66,6 +84,7 @@ function random_bg_color() {
   // Set the background to that color
   document.body.style.background = bgColor;
 }
+
 
 function loadTrack(track_index) {
   clearInterval(updateTimer);
@@ -116,3 +135,45 @@ function nextTrack() {
   loadTrack(track_index);
   playTrack();
 }
+function seekTo() {
+  // Calculate the seek position by the
+  // percentage of the seek slider
+  // and get the relative duration to the track
+  seekto = curr_track.duration * (seek_slider.value / 100);
+ 
+  // Set the current track position to the calculated seek position
+  curr_track.currentTime = seekto;
+}
+ 
+function setVolume() {
+  // Set the volume according to the
+  // percentage of the volume slider set
+  curr_track.volume = volume_slider.value / 100;
+}
+function seekUpdate() {
+  let seekPosition = 0;
+ 
+  // Check if the current track duration is a legible number
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+    seek_slider.value = seekPosition;
+ 
+    // Calculate the time left and the total duration
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+ 
+    // Add a zero to the single digit time values
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+ 
+    // Display the updated duration
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
+// Load the first track in the tracklist
+loadTrack(track_index);
